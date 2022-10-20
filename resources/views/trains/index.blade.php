ac267e71d8d798e84608dd6c272e4febacca98aa
n00210357 <99271838+n00210357@users.noreply.github.com>
2022-10-20 18:02:38 +0100

    controller

---
+++ resources/views/trains/index.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Trains') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a href="{{ route('trains.create') }}" class="btn-link btn-lg mb-2">+ New Train</a>
+            @forelse ($trains as $train)
+                <div class="p-6 bg-white border-b border-gray-200 shadow-sj sm:rounded-lg">
+                    <h2>
+                        <a href="{{ route('trains.show', $trains->id) }}"> {{$train->name}}</a>
+                    </h2>
+                    <p class="mt-2">
+                       {{Str::limit($train->destination), 200}}
+                    </p>
+                    <span class="block mt-4 text-sm opacity-70"> {{$train->updated_at->diffForHumans()}}</span>
+                </div>
+                @empty
+                <p>You have no trains</p>
+                @endforelse
+                {{$trains->links()}}
+            </div>
+        </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('/trains', TrainController::class)->middleware(['auth']);
 
 require __DIR__.'/auth.php';
